58729b8a069d32e05d8dd0e233aa598a3ccd1abc
fulder <misad90@gmail.com>
2020-05-26 22:53:53 +0200

    Remove empty space

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,6 @@
 mkdir -p /var/madsonic
 
 # Download and unpack tar.gz standalone version
-
 fetch https://download.madsonic.org/6.2/20190425_madsonic-6.2.9084-standalone.tar.gz -o madsonic.tar.gz
 tar xf madsonic.tar.gz -C /var/madsonic
 
