9432da099641055c03a3eb9b52cf963466a0a5e6
fulder <misad90@gmail.com>
2020-05-26 22:48:04 +0200

    Cleanup 0.0.1.sh migration

--- overlay/usr/local/migrations/0.0.1.sh
+++ overlay/usr/local/migrations/0.0.1.sh
@@ -3,3 +3,6 @@
 # upgrade to 7.0.10390
 fetch https://madsonic.org/download/7.0/20190510_madsonic-7.0.10390-standalone.tar.gz -o madsonic.tar.gz
 tar xf madsonic.tar.gz -C /var/madsonic
+
+# cleanup
+rm madsonic.tar.gz
