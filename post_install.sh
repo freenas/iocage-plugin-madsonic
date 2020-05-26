d12cc565687b20d11029b5e040ef6d1f75135bfa
fulder <misad90@gmail.com>
2020-05-26 22:47:07 +0200

    Skip installing transcode including ffmpeg

--- post_install.sh
+++ post_install.sh
@@ -8,10 +8,6 @@ mkdir -p /var/madsonic
 fetch https://download.madsonic.org/6.2/20190425_madsonic-6.2.9084-standalone.tar.gz -o madsonic.tar.gz
 tar xf madsonic.tar.gz -C /var/madsonic
 
-# Download and unzip transcode package
-fetch https://download.madsonic.org/transcode/20190425_madsonic-transcode-linux-x86.zip -o madsonic-transcode.zip
-unzip madsonic-transcode.zip -d /var/madsonic/
-
 # Enable and start service
 sysrc -f /etc/rc.conf madsonic_enable="YES"
 service madsonic start 2>/dev/null
