d090842171043f9b0da5843e8934ce85076b15b3
fulder <misad90@gmail.com>
2020-05-26 20:36:59 +0200

    Install madsonic from tar.gz package

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-fetch https://www.madsonic.org/pages/request.jsp?branch=6.2&target=20190425_madsonic-6.2.9084-standalone.tar.gz
+# Create madsonic dir
+mkdir -p /var/madsonic
+
+# Download and unpack tar.gz standalone version
+fetch https://download.madsonic.org/6.2/20190425_madsonic-6.2.9084-standalone.tar.gz -o madsonic.tar.gz
+tar xf madsonic.tar.gz -C /var/madsonic
+
+# Download and unzip transcode package
+fetch https://download.madsonic.org/transcode/20190425_madsonic-transcode-linux-x86.zip -o madsonic-transcode.zip
+unzip madsonic-transcode.zip -d /var/madsonic/
+
 
 
