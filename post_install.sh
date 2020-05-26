67e5a1ea74f3ee1001aab3e8be309f2008cc1176
fulder <misad90@gmail.com>
2020-05-26 22:28:23 +0200

    Update madsonic to latest version

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,8 @@
 mkdir -p /var/madsonic
 
 # Download and unpack tar.gz standalone version
-fetch https://download.madsonic.org/6.2/20190425_madsonic-6.2.9084-standalone.tar.gz -o madsonic.tar.gz
+
+fetch https://madsonic.org/download/7.0/20190510_madsonic-7.0.10390-standalone.tar.gz -o madsonic.tar.gz
 tar xf madsonic.tar.gz -C /var/madsonic
 
 # Download and unzip transcode package
