c51bd2a94828d87e3632f3e27de000f8ef229a63
fulder <misad90@gmail.com>
2020-05-26 23:10:51 +0200

    Cleanup post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,13 +6,11 @@ mkdir -p /var/madsonic
 # Download and unpack tar.gz standalone version
 fetch https://download.madsonic.org/6.2/20190425_madsonic-6.2.9084-standalone.tar.gz -o madsonic.tar.gz
 tar xf madsonic.tar.gz -C /var/madsonic
-
-# Enable and start service
-sysrc -f /etc/rc.conf madsonic_enable="YES"
-service madsonic start 2>/dev/null
-
-# Cleanup
 rm madsonic.tar.gz
 
 # Run migrations
 /usr/local/migrations/0.0.1.sh
+
+# Enable and start service
+sysrc -f /etc/rc.conf madsonic_enable="YES"
+service madsonic start 2>/dev/null
